2d08b08087a9ccad9b9b6469d4a9ec08aba40586
GPHNZ <94904867+GPHNZ@users.noreply.github.com>
2022-12-25 18:56:44 +0100

    update suspend counters

--- ESP/Week1/Opdracht5/Opdracht5.ino
+++ ESP/Week1/Opdracht5/Opdracht5.ino
@@ -1,85 +1,84 @@
-// basic_tasks.ino but name changed to opdracht 5
-// MIT license 
-
-#define LED1 15
-#define LED2 16
-#define LED3 17
-
+// basic_tasks.ino
+// MIT License (see file LICENSE)
+// Change the following if you want to use
+// different GPIO pins for the three LEDs
+#define LED1 12 // GPIO 12
+#define LED2 14 // etc.
+#define LED3 27
 struct s_led {
-  byte gpio;
-  byte state;
-  unsigned napms;
-  TaskHandle_t taskh;
+  byte gpio; // LED GPIO number
+  byte state; // LED state
+  unsigned napms; // Delay to use (ms)
+  TaskHandle_t taskh; // Task handle
 };
 
 static s_led leds[3] = {
-  {LED1, 0, 500, 0},
-  {LED2, 0, 250, 0},
-  {LED3, 0, 750, 0}
+  { LED1, 0, 500, 0 },
+  { LED2, 0, 200, 0 },
+  { LED3, 0, 750, 0 }
 };
 
-static void led_task_func(void *argp){
+static void led_task_func(void *argp) {
   s_led *ledp = (s_led*)argp;
   unsigned stack_hwm = 0, temp;
-  
   delay(1000);
-
-  for(;;){
-    digitalWrite(ledp->gpio, ledp->state ^= 1);
+  for (;;) {
+    digitalWrite(ledp->gpio,ledp->state ^= 1);
     temp = uxTaskGetStackHighWaterMark(nullptr);
-    if(!stack_hwm || temp < stack_hwm){
-      stack_hwm = temp;
-      printf("Task for GPIO %d has stack hwm %u\n",ledp->gpio,stack_hwm);
+  if ( !stack_hwm || temp < stack_hwm ) {
+    stack_hwm = temp;
+    printf("Task for gpio %d has stack hwm %u\n",
+    ledp->gpio,stack_hwm);
     }
-    delay(ledp->napms);
+  delay(ledp->napms);
   }
 }
 
-void setup(){
-  int app_cpu = 0;
-  delay(500);
-
+void setup() {
+  int app_cpu = 0; // CPU number
+  delay(500); // Pause for serial setup
   app_cpu = xPortGetCoreID();
-  printf("App_cpu is %d (%s core)\n",app_cpu,app_cpu > 0 ? "Dual" : "Single");
-
+  printf("app_cpu is %d (%s core)\n",
+  app_cpu,
+  app_cpu > 0 ? "Dual" : "Single");
   printf("LEDs on gpios: ");
-  for(auto& led : leds){
+  for ( auto& led : leds ) {
     pinMode(led.gpio,OUTPUT);
     digitalWrite(led.gpio,LOW);
     xTaskCreatePinnedToCore(
-      led_task_func,
-      "led_task",
-      2048,
-      &led,
-      1,
-      &led.taskh,
-      app_cpu
-     );
-     printf("%d ",led.gpio);
+    led_task_func,
+    "led_task",
+    2048,
+    &led,
+    1,
+    &led.taskh,
+    app_cpu
+    );
+    printf("%d ",led.gpio);
+    putchar('\n');
   }
-  putchar('\n');
 }
-int ctr = 0;
+int counter = 0;
 void loop() {
-  printf("counter = %d\n", ctr);
-  if (ctr == 10) {
+  printf("counter = %d\n", counter);
+  if (counter == 10) {
     vTaskSuspend(leds[0].taskh);
-    printf ("Suspend Task %d",(ctr/3));
+    printf ("Suspend Task %d",(counter/3));
   }
-  if (ctr == 13) {
+  if (counter == 13) {
     vTaskSuspend(leds[1].taskh);
-    printf ("Suspend Task %d",(ctr/3));
+    printf ("Suspend Task %d",(counter/3));
   }
-  if (ctr == 16) {
+  if (counter == 16) {
     vTaskSuspend(leds[2].taskh);
-    printf ("Suspend Task %d",(ctr/3));
+    printf ("Suspend Task %d",(counter/3));
   }
-  if (ctr == 25) {
+  if (counter == 19) {
     vTaskResume(leds[0].taskh);
     vTaskResume(leds[1].taskh);
     vTaskResume(leds[2].taskh);
-    ctr = 0;
+    counter = 0;
   }
   delay(1000);
-  ctr++;
+  counter++;
 }
